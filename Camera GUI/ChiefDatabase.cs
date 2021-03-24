3c99a61b65d3b9adba8f55400a8fffbbd2e92794
jacksonjh <Nicholas@jhtester.com.au>
2021-03-25 09:44:53 +1100

    cleaning up code and adding comments 1

--- Camera GUI/ChiefDatabase.cs
+++ Camera GUI/ChiefDatabase.cs
@@ -9,31 +9,53 @@ namespace Camera_GUI
 {
     public class ChiefDatabase : JPLC_BASE
     {
+        // An output, to whether the flash is enabled or not
         [Order(1)]
         public JPLCProperty<bool> flash_state { get; set; }
+
+        // An output, setting the zero for the X axis
         [Order(2)]
         public JPLCProperty<float> x_out { get; set; }
+
+        // An output, setting the zero for the Y axis
         [Order(3)]
         public JPLCProperty<float> y_out { get; set; }
+
+        // An output, setting the target angle for the camera to pan to
         [Order(4)]
         public JPLCProperty<float> pan_out { get; set; }
+
+        // An input, showing the current angle the camera is currently panned to
         [Order(5)]
         public JPLCProperty<float> pan_in { get; set; }
+
+        // An input, showing the current incline of the X axis
         [Order(6)]
         public JPLCProperty<float> x_in { get; set; }
+
+        // An input, showing the current incline of the Y axis
         [Order(7)]
         public JPLCProperty<float> y_in { get; set; }
+
+        // An input, showing the interative count of the flashes that have gone off
         [Order(8)]
         public JPLCProperty<short> flash_count { get; set; }
+
+        // An input, showing the current state of the cooling systems, driven by the temprature of the camera
         [Order(9)]
         public JPLCProperty<bool> cool_state { get; set; }
+
+        // An input, showing the current temperature of the camera
         [Order(10)]
         public JPLCProperty<float> temperature { get; set; }
+
+        // An input, showing the estimated remaining percentage of the battery
         [Order(11)]
         public JPLCProperty<float> battery_level { get; set; }
+
+        // An input, showing whether the battery is charging or not
         [Order(12)]
         public JPLCProperty<bool> charge_state { get; set; }
-
 
         public ChiefDatabase(int address = 0) : base(address) {  }
     }
--- Camera GUI/IOHome.xaml.cs
+++ Camera GUI/IOHome.xaml.cs
@@ -30,6 +30,8 @@ namespace Camera_GUI
 	/// </summary>
 	public partial class IOHome : Page
 	{
+		// A large amount of variables, all meant for temporary storage for data being read, to being set in the GUI
+		// Some of these could be removed or combined, they're rather inconsistent
 		public bool flash_state;
 		public bool cool_state;
 		public double angle_x;
@@ -48,6 +50,7 @@ namespace Camera_GUI
 		float old_delta;
 		float new_delta;
 
+		// This function is called to alter the LED bar at the top, is currently unused
 		private void FlashButton_Checked(object sender, RoutedEventArgs e)
 		{
 			if (FlashButton.IsChecked == true)
@@ -63,6 +66,7 @@ namespace Camera_GUI
 			FlashButton.Content = flash_text;
 		}
 
+		// This function is somewhat incomplete, removing the focus from GUI elements when the escape key is pressed
 		private void EscapePressed(object sender, System.Windows.Input.KeyEventArgs e)
 		{
 			if (e.Key == Key.Escape)
@@ -70,6 +74,7 @@ namespace Camera_GUI
 				Keyboard.ClearFocus();
 			}
 		}
+		// When enter is pressed while a value is written in the "Set X" field, this sets the data in a variable to be sent later
 		private void XReturnPressed(object sender, System.Windows.Input.KeyEventArgs e)
 		{
 			if (e.Key == Key.Return)
@@ -78,6 +83,7 @@ namespace Camera_GUI
 				XSetField.Text = "";
 			}
 		}
+		// When enter is pressed while a value is written in the "Set Y" field, this sets the data in a variable to be sent later
 		private void YReturnPressed(object sender, System.Windows.Input.KeyEventArgs e)
 		{
 			if (e.Key == Key.Return)
@@ -86,6 +92,7 @@ namespace Camera_GUI
 				YSetField.Text = "";
 			}
 		}
+		// When enter is pressed while a value is written in the "Set Pan" field, this sets the data in a variable to be sent later
 		private void PanReturnPressed(object sender, System.Windows.Input.KeyEventArgs e)
         {
 			if (e.Key == Key.Return)
@@ -95,6 +102,7 @@ namespace Camera_GUI
             }
 		}
 
+		// The base function, called when iniating the window. First connects to the PLC, then opens the window
 		public IOHome()
 		{
 			// Connecting to PLC
@@ -104,10 +112,12 @@ namespace Camera_GUI
 			InitializeComponent();
 		}
 
+		// This command takes the temporary buffer variables and assigns them to GUI elements, refreshing them in their own thread
 		public void UpdateGUI(ChiefDatabase CB)
         {
 			Dispatcher.Invoke(delegate
 			{
+				// IF/ELSE statements controlling whether the GUI element is on or off
 				if (flash_state is false)
 				{
 					LEDIndicator.Text = LEDOff;
@@ -120,6 +130,7 @@ namespace Camera_GUI
 					LEDIndicator.Background = new SolidColorBrush(Color.FromRgb(173, 220, 255));
 					LEDIndicator.Foreground = new SolidColorBrush(Color.FromRgb(179, 142, 86));
 				}
+				// Similarly, these statements will update the gui whether the cooling system is on or off
 				if (cool_state is true)
 				{
 					CameraTempStatus.Foreground = Brushes.PowderBlue;
@@ -132,7 +143,8 @@ namespace Camera_GUI
 					string not_cooling = "Not Cooling";
 					CameraTempStatus.Text = not_cooling;
 				}
-
+				
+				// The following lines are copying data from the storage variables, and writing it onto the gui
 				AngleXText.Text = angle_x.ToString() + " degrees";
 				AngleYText.Text = angle_y.ToString() + " degrees";
 				PanReadBox.Text = pan_reading.ToString() + " degrees";
@@ -141,12 +153,14 @@ namespace Camera_GUI
 				BatteryChargeBox.Text = battery_level.ToString() + "%";
 				BatteryChargeBox.Width = Math.Round(battery_level*4.75, 0);
 
+				// In order to get the target angles, the strings in the x, y and pan fields are parsed to floats
 				float.TryParse(XSetBox.Text, out x_set_box);
 				float.TryParse(YSetBox.Text, out y_set_box);
 				float.TryParse(PanSetBox.Text, out pan_set_box);
 			});
 		}
 
+		// This function primarily deals with establishing a connection with the Siemens 1215 PLC
 		public void ConnectToPLC()
         {
 			// Connecting to PLC
@@ -154,13 +168,16 @@ namespace Camera_GUI
 			double timeBetweenRetries = 0.06; // seconds
 			double timeBetweenReads = 0.05; //seconds
 
-			// Setup program
+			// Setup program, which creates a database object, references the database, and uses a connect Function
 			var chiefDB = new ChiefDatabase();
 			var acclinDBNum = 4;
 			plc.Connect();
+			// This function is an inital read from the database
 			chiefDB.ReadFromDB(plc, acclinDBNum);
 
-			// READ PLC
+			// -- READ PLC --
+			// This is an observable object, which is important when dealing with asynchronous data
+			// The observable will feed data back to an observer to be analysed
 			var readDBObservable = Observable.Create<ChiefDatabase>(o =>
 			{
 				var result = chiefDB.ReadFromDB(plc, acclinDBNum);
@@ -200,6 +217,7 @@ namespace Camera_GUI
 				.Repeat()
 			);
 
+			// A disposable variable that subscribes a set of commands to a scheduler, which executes every so often
 			var disposable = observable
 				.ObserveOn(System.Reactive.Concurrency.TaskPoolScheduler.Default)
 				.RetryWhen(errors => errors.SelectMany(Observable.Timer(TimeSpan.FromSeconds(timeBetweenRetries))))
@@ -212,7 +230,8 @@ namespace Camera_GUI
 					Console.WriteLine("\n" + $"Reading from PLC Beam / Fault DataBase on {DateTime.Now}");
 					Console.WriteLine(chiefdb.flash_state.Value);
 
-					// Reading
+					// -- READING --
+					// The following lines are taking data from the database object, and copying them to global variables
 					angle_x = Math.Round(chiefdb.x_out.Value,1);
 					angle_y = Math.Round(chiefdb.y_out.Value,1);
 					pan_reading = Math.Round(chiefdb.pan_out.Value,1);
@@ -220,21 +239,27 @@ namespace Camera_GUI
 					temperature = Math.Round(chiefdb.temperature.Value, 1);
 					cool_state = chiefdb.cool_state.Value;
 					battery_level = Math.Round(chiefdb.battery_level.Value,0);
-					//Writing
+
+					// -- WRITING --
+					// First, the GUI will update the gui based off the most recent data, so that the PLC can recieve the newest data
 					UpdateGUI(chiefdb);
+					// The next lines copy data into  the database 
 					chiefdb.flash_state.Value = flash_state;
 					chiefdb.x_in.Value = x_set_box;
 					chiefdb.y_in.Value = y_set_box;
 					chiefdb.pan_in.Value = pan_set_box;
 
+					// delta is a concept that minimises bandwidth across the connection from the PC to the PLC
+					// This first line adds up data to a non-important figure
 					new_delta = x_set_box + y_set_box + pan_set_box + Convert.ToInt32(flash_state);
+					// If new_delta has changed since the last measurement, then the given command/s are executed
 					if(new_delta != old_delta)
                     {
 						chiefDB.WriteToDB(plc, acclinDBNum);
 					}
+					// To ensure the difference between values can be measured, the old value must be saved for future reference
+					// This is done after the comparison so that by the time the next comparison happens, the old and new deltas might be different
 					old_delta = new_delta;
-					
-					Console.WriteLine("\n" + "---------------------------------------------------------" + "\n");
 				});
 		}
     }
